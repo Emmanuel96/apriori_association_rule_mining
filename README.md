c7e42047bf34a316a65c4ea2acdc25c0ce6dc169
Emmanuel Audu <eaudu@gidimobile.com>
2020-06-11 18:40:10 +0100

    edited readme.md file

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# apriori_association_rule_mining
-Find association rules For Student Dataset With Apriori Association Rule Mining Algorithm
+# Association Rule Mining With Student Dataset
+
+**Aim:** The aim here is to demonstrate one application of Association Rule mining with the Apriori Algorithm.
+
+## Project Description
+
+Association rule mining is a very important supervised machine learning method. It's used to find the relationships between different features and this in turn can be used to set association rules. An example application of association rule would be Amazon's suggestion system.
+
+In the simplest form Amazon makes use of their users baskets data to come up with different rules stating what other items a user is likely to buy based on what she/he has already purchased.
+
+In this case, we use the algorithm to find the rules and relationships that exists amongst a student dataset.
+
+## Requriments
+
+To run this project you will need to install the following installed:
+
+1. Python 3.5 and above
+2. mlxtend
+3. Numpy
+4. Pandas
+
+## Project Installation
+
+Make sure you have installed all the requirements, then follow the instruction below:
+
+    git clone https://github.com/Emmanuel96/apriori_association_rule_mining.git
+    cd apriori_association_rule_mining
+
+## License
+
+This project is licensed under the terms of the MIT license Copyright &copy; 2020.
--- apriori.py
+++ apriori.py
@@ -7,15 +7,16 @@ Description: Using the Apriori algorithm to get the top 10 rules for a student d
 
 
 # pandas, numpy ans seaborne
-import matplotlib.pyplot as plt 
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 
 # association rule and apriori algorithm
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 
 # function to convert categorical data to dummy data
+
+
 def handle_cat_data(cat_feats, data):
     for f in cat_feats:
         to_add = pd.get_dummies(data[f], prefix=f, drop_first=True)
@@ -34,12 +35,12 @@ student_data = pd.DataFrame(data)
 # drop all null data
 student_data.dropna(inplace=True)
 
-cat_data = ['reason','famrel','freetime','goout','higher','Medu', 'guardian', 'traveltime','studytime', 'failures', 'schoolsup', 'famsup', 'fatherd', 'activities','absences','nursery','internet','romantic','Dalc', 'Walc', 'health', 'absences', 'G1', 'G2', 'G3',
-'nursery','school', 'sex', 'age', 'address', 'famsize', 'Pstatus', 'Fedu', 'Mjob', 'Fjob']
+cat_data = ['reason', 'famrel', 'freetime', 'goout', 'higher', 'Medu', 'guardian', 'traveltime', 'studytime', 'failures', 'schoolsup', 'famsup', 'fatherd', 'activities', 'absences', 'nursery', 'internet', 'romantic', 'Dalc', 'Walc', 'health', 'absences', 'G1', 'G2', 'G3',
+            'nursery', 'school', 'sex', 'age', 'address', 'famsize', 'Pstatus', 'Fedu', 'Mjob', 'Fjob']
 
 student_data = handle_cat_data(cat_data, student_data)
 
-freq_item_set = apriori(student_data, min_support = 0.07, use_colnames = True)
+freq_item_set = apriori(student_data, min_support=0.07, use_colnames=True)
 
 rules = association_rules(freq_item_set, metric="lift", min_threshold=1)
 print(rules.head())
